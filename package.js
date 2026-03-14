6012ebb7ac39962018a140939e7cc8d7f87a7a5a
m65234899-cloud <m65234899@gmail.com>
2026-03-14 05:40:10 +0300

    Initialize package.json for bank-bot project

--- package.js
+++ package.js
@@ -1,1 +1,13 @@
-
+{
+  "name": "bank-bot",
+  "version": "1.0.0",
+  "description": "بوت bank بسيط لدسكورد",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "discord.js": "^14.14.0",
+    "dotenv": "^16.3.1"
+  }
+}
